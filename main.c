dc66ff8e39ab6a8913a28e9775b87a4f2b129482
Paweł Łaskarzewski <gamespabloplay@gmail.com>
2022-12-14 01:31:05 +0100

    f

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-void draw_table(int board[]) {
+#define N 9
+void draw_table(int board[N][N]) {
 	printf("- - - - - - -\n");
 	for (int i = 0; i < 9; i++) {
 		printf("|");
 		for (int j = 0; j < 9; j++) {
-			if (board[9 * i + j] == 0) {
+			if (board[i][j] == 0) {
 				printf("-");
 			}
 			else {
-				printf("%d", board[9 * i + j]);
+				printf("%d", board[i][j]);
 			}
 			if (j % 3 == 2) {
 				printf("|");
@@ -23,33 +24,46 @@ void draw_table(int board[]) {
 		printf("\n");
 	}
 }
-void asign_value(int board[],int value,int row, int column) {
-	board[9 * row + column] = value;
+void asign_value(int board[N][N],int value,int row, int column) {
+	board[row][column] = value;
 }
-void insert_to_table(int board[]) {
+void ask_for_value(int board[N][N], int* value, int* row, int* column) {
 	bool istrue = false;
-	int value=0;
-	int row=0, column=0;
-	while (istrue!=true) {
-
+	while (istrue != true) {
 		printf("Insert value from %d to %d:", 1, 9);
-		scanf_s("%d", &value);
+		scanf_s("%d", value);
 		printf("Which row from %d to %d:", 1, 9);
-		scanf_s("%d", &row);
+		scanf_s("%d", row);
 		printf("Which column from %d to %d:", 1, 9);
-		scanf_s("%d", &column);
-		if (value < 1 || value>9|| row < 1 || row>9|| column < 1 || column>9) {
+		scanf_s("%d", column);
+		if (*value < 1 || *value>9 || *row < 1 || *row>9 || *column < 1 || *column>9) {
 			printf("You entered wrong values.Please try again.\n");
 		}
 		else {
 			istrue = true;
 		}
 	}
+}
+void insert_to_table(int board[N][N]) {
+	int row = 0, column = 0, value = 0;
+	ask_for_value(board,&value,&row,&column);
 	asign_value(board, value, row-1, column-1);
 }
 void main() {
-	int board[81];
-	memset(board,0,sizeof(board));
+	/*int board[N][N] = {
+		{0,0,7,4,9,1,6,0,5},
+		{2,0,0,0,6,0,3,0,9},
+		{0,0,0,0,0,7,0,1,0},
+		{0,5,8,6,0,0,0,0,4},
+		{0,0,3,0,0,0,0,9,0},
+		{0,0,6,2,0,0,1,8,7},
+		{9,0,4,0,7,0,0,0,2},
+		{6,7,0,8,3,0,0,0,0},
+		{8,1,0,0,4,5,0,0,0} 
+	};*/
+	
+	int board[N][N];
+	memset(board, 0, sizeof(board));
 	bool istrue = false;
 	int go_on;
 	while (istrue != true) {
